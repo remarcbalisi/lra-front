d069c216ad5bd49d948ceef334c319ececdd6699
Remarc Balisi <remarc.balisi@gmail.com>
2020-12-06 04:42:17 +0800

    added padding

--- src/App.js
+++ src/App.js
@@ -6,23 +6,28 @@ import {
 } from "react-router-dom"
 import Login from "./pages/auth";
 import Home from "./pages/home";
-import Navbar from "./components/navbar";
 import './App.css';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 const App = () => {
   return (
-    <div>
-      <Router>
-      <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-        </Switch>
-    </Router>
-    </div>
+    <Layout>
+      <Header></Header>
+      <Content>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </Content>
+      <Footer>Created with love</Footer>
+    </Layout>
   );
 }
 
--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,22 +1,16 @@
-import { Form, Input, Button, Checkbox } from 'antd';
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
-const tailLayout = {
-  wrapperCol: {
-    offset: 8,
-    span: 16,
-  },
-};
+import {
+  Row,
+  Col,
+  Form,
+  Input,
+  Button,
+  Checkbox
+} from 'antd';
 
 const Login = () => {
   const onFinish = (values) => {
     console.log('Success:', values);
+    
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -24,51 +18,54 @@ const Login = () => {
   };
 
   return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <Row justify="center" align="middle" style={{ paddingTop: "2%" }}>
+      <Col span={12}>
+        <Form
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
   );
 };
 
