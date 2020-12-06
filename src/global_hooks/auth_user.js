dd2a257c46f92eab18d2fc1865bdd04b9f6926bb
Remarc Balisi <remarc.balisi@gmail.com>
2020-12-06 12:51:41 +0800

    home users

--- src/global_hooks/auth_user.js
+++ src/global_hooks/auth_user.js
@@ -4,12 +4,20 @@ import API from "../helpers/api"
 
 const initialState = {
   user: null,
+  users: []
 };
 
 const actions = {
   checkAuth: async (store) => {
     const {data: {data}} = await API.get('auth/user')
     store.setState({ user: data })
+  },
+  getUsers: async(store) => {
+    const {data: {data}} = await API.get('admin/user')
+    store.setState({
+      ...store.state,
+      users: data
+    })
   }
 };
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,56 @@
+import {
+  Row,
+  Col,
+  Table,
+  Space
+} from 'antd';
+import useGlobalAuthUser from "../../global_hooks/auth_user"
+import {useEffect} from "react"
+
 const Home = () => {
-    return (
-        <h1>Home</h1>
-    )
+  const [auth, authActions] = useGlobalAuthUser();
+  const { Column, ColumnGroup } = Table;
+
+  const users_columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <a>Edit {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    authActions.getUsers()
+  }, []);
+
+  return (
+    <Row 
+      justify="center" 
+      align="middle" 
+      style={{ paddingTop: "2%", height: "100vh", backgroundColor: "white" }}>
+        <Table dataSource={auth.users} columns={users_columns} rowKey="id"/>
+    </Row>
+  )
 }
 
 export default Home
